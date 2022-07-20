0dedd72cdb5cdb4a664462630a9711a9e017616e
Rishabh Mishra <rishabh@WFX-LAP-593.local>
2022-07-20 17:22:49 +0530

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+// scrollIndicator starts
+
+window.addEventListener('scroll', moveScrollIndicator);
+
+const scrollIndicatorElt = document.getElementById('scrollIndicator');
+const maxHeight = window.document.body.scrollHeight - window.innerHeight;
+
+function moveScrollIndicator(e) {
+  const percentage = ((window.scrollY) / maxHeight) * 100;
+
+  scrollIndicatorElt.style.width = percentage + '%';
+}
+
+// scrollIndicator ends
